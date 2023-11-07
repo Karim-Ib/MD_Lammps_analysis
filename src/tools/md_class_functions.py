fixed rdf scaling, made code more readable

src/tools/md_class_functions.py
@@ -324,11 +324,12 @@ def get_sphere_volume(r: float) -> float:
     :param r: radius of the sphere
     :return: volume
     '''
-    return 4 * np.pi * r**3 / 4
+    return 4 * np.pi * r**3 / 3
 
 
 def init_rdf(data: np.ndarray, box: np.ndarray, n_bins: int, start: float,
-             stop: float=None) ->(np.ndarray, float, cKDTree, np.ndarray, np.ndarray):
+             stop: float=None, data_2: np.ndarray=None):
+
     '''
     Initializes Objects to calculate the rdf
     :param data: input data
@@ -336,6 +337,7 @@ def init_rdf(data: np.ndarray, box: np.ndarray, n_bins: int, start: float,
     :param n_bins: number of bins
     :param start: start radius
     :param stop: end radius
+    :param data_2: optional, positions of 2nd particle type
     :return: returns the scaled data, number density, the periodic tree, the bin list and bin volumes
     '''
 
@@ -350,12 +352,16 @@ def init_rdf(data: np.ndarray, box: np.ndarray, n_bins: int, start: float,
 
     bin_list = np.linspace(start, stop, n_bins)
     bin_vol = np.zeros(len(bin_list))
-    bin_vol[0] = get_sphere_volume(bin_list[0])
 
-    for _bin in range(1, len(bin_vol)):
-        bin_vol[_bin] = get_sphere_volume(bin_list[_bin]) - bin_vol[_bin - 1]
 
-    return upscale, number_density, tree, bin_list, bin_vol
+    for _bin in range(len(bin_vol)):
+        bin_vol[_bin] = get_sphere_volume(bin_list[_bin]) - get_sphere_volume(bin_list[_bin - 1])
+
+    if data_2 is None:
+        return upscale, number_density, tree, bin_list, bin_vol, None
+    else:
+        upscale_2 = scale_to_box(data_2[:, 2:], box)
+        return upscale, upscale_2, upscale_2.shape[0]/Vol, tree, bin_list, bin_vol
 
 
 def calculate_rdf(data: np.ndarray, rho: float, tree: cKDTree, bins: np.ndarray,
@@ -392,21 +398,43 @@ def calculate_rdf(data: np.ndarray, rho: float, tree: cKDTree, bins: np.ndarray,
 
     return count/rho, bins
 
-def get_all_distances(data: np.ndarray, box: []=None) -> np.ndarray:
+def get_all_distances(data: np.ndarray, box: []=None, data_2: np.ndarray=None) -> np.ndarray:
+    if data_2 is None:
+        distances = np.zeros((data.shape[0], data.shape[0]))
 
-    distances = np.zeros((data.shape[0], data.shape[0]))
+        for atom in range(data.shape[0]):
+            for neighbour in range(data.shape[0]):
+                distances[atom, neighbour] = get_distance(data[atom, :], data[neighbour, :],
+                                                          mode="pbc", box=box)
+        return distances
+    else:
+        distances = np.zeros((data.shape[0], data_2.shape[0]))
+        for o_atom in range(data.shape[0]):
+            for h_atom in range(data_2.shape[0]):
+                distances[o_atom, h_atom] = get_distance(data[o_atom, :], data_2[h_atom, :],
+                                                         mode="pbc", box=box)
+        return distances
 
-    for atom in range(data.shape[0]):
-        for neighbour in range(data.shape[0]):
-            distances[atom, neighbour] = get_distance(data[atom, :], data[neighbour, :],
-                                                      mode="pbc", box=box)
-    return distances
 
 def count_rdf_hist(distances: np.ndarray, bins: np.ndarray):
 
     counter = np.zeros(bins.shape[0] - 1)
 
     for atom in range(distances.shape[0]):
-        temp, _ = np.histogram(distances[atom, :], bins=bins)
+        temp, _ = np.histogram(distances[atom, :], bins=bins, density=False)
         counter += temp
+
     return counter / distances.shape[0]
+
+
+def calc_rdf_rdist(data: [np.ndarray], box: [np.ndarray], data_2: [np.ndarray]=None, snapshot: int=0,
+                  n_bins: int=50, start: float=0.01, stop: float=None)-> (np.ndarray, np.ndarray):
+
+    upscale, number_density, _, bin_list, bin_vol, upscale_2 = init_rdf(data[snapshot], box[snapshot],
+                                                             n_bins, start, stop, data_2)
+
+    distance = get_all_distances(upscale, box[snapshot], upscale_2)
+    counter = count_rdf_hist(distance, bins=bin_list)
+    counter = np.divide(counter, bin_vol[1:])
+
+    return counter / number_density, bin_list[1:]

src/water_md_class.py
@@ -558,11 +558,6 @@ class Trajectory:
 
     def get_rdf(self, snapshot: int=0, gr_type: str="OO", n_bins: int=50,
                 start: float=0.01, stop: float= None, n_parallel: int=4, single=False) -> (np.ndarray, np.ndarray):
-        '''
-        Method to calculate the radial distribution function. Wraps around the rdfpy package from
-        Batuhan Yildirim and Hamish Galloway Brown
-        :return:
-        '''
 
         if single:
             if gr_type == "OO":
@@ -572,13 +567,20 @@ class Trajectory:
 
                 return gr, bin_list
 
+
             if gr_type == "HH":
+                upscale, number_density, tree, bin_list, bin_vol = init_rdf(self.s1[snapshot], self.box_size[snapshot],
+                                                                            n_bins, start, stop)
+                gr, _ = calculate_rdf(upscale, number_density, tree, bin_list, bin_vol, n_cores=n_parallel)
 
-                return None
+                return gr, bin_list
+
             if gr_type == "OH":
+                upscale, number_density, tree, bin_list, bin_vol = init_rdf(self.s1[snapshot], self.box_size[snapshot],
+                                                                            n_bins, start, stop)
+                gr, _ = calculate_rdf(upscale, number_density, tree, bin_list, bin_vol, n_cores=n_parallel)
 
-                return None
-
+                return gr, bin_list
         if not single:
             if gr_type == "OO":
 
@@ -591,9 +593,9 @@ class Trajectory:
 
                     gr_data[snap, :] = temp
 
-            gr = np.sum(gr_data, axis=0)
+            gr = np.sum(gr_data, axis=0) / self.n_snapshots
 
-            return gr/self.n_snapshots, bin_list
+            return gr, bin_list
 
 
             if gr_type == "HH":
@@ -604,36 +606,51 @@ class Trajectory:
                 return None
 
 
-
     def get_rdf_rdist(self, snapshot: int=0, gr_type: str="OO", n_bins: int=50, start: float=0.01, stop: float=None,
-                      single=False)-> (np.ndarray, np.ndarray):
-        if single:
+                      single_frame=False)-> (np.ndarray, np.ndarray):
+        if single_frame:
             if gr_type=="OO":
-                upscale, number_density, _, bin_list, bin_vol = init_rdf(self.s2[snapshot], self.box_size[snapshot],
-                                                                            n_bins, start, stop)
+                gr, r = calc_rdf_rdist(data=self.s2, box=self.box_size, snapshot=snapshot, n_bins=n_bins, start=start,
+                                       stop=stop)
+                return gr, r
+            if gr_type=="HH":
+                gr, r = calc_rdf_rdist(data=self.s1, box=self.box_size, snapshot=snapshot, n_bins=n_bins, start=start,
+                                       stop=stop)
+                return gr, r
+            if gr_type=="OH":
+                gr, r = calc_rdf_rdist(data=self.s2, box=self.box_size, snapshot=snapshot, n_bins=n_bins, start=start,
+                                       stop=stop, data_2=self.s1[snapshot])
+                return gr, r
 
-                distance = get_all_distances(upscale, self.box_size[snapshot])
-                counter = count_rdf_hist(distance, bins=bin_list)
-                counter = np.divide(counter, bin_vol[:-1])
-
-            return counter / number_density, bin_list
-
-        if not single:
+        if not single_frame:
             if gr_type=="OO":
                 rdf_list = np.zeros((self.n_snapshots, n_bins -1))
                 for snap in range(self.n_snapshots ):
-                    upscale, number_density, _, bin_list, bin_vol = init_rdf(self.s2[snap], self.box_size[snap],
-                                                                             n_bins, start, stop)
 
-                    distance = get_all_distances(upscale, self.box_size[snap])
-                    counter = count_rdf_hist(distance, bins=bin_list)
-                    counter = np.divide(counter, bin_vol[1:])
-                    rdf_list[snap, :] = counter
+                    gr, r = calc_rdf_rdist(data=self.s2, box=self.box_size, snapshot=snap, n_bins=n_bins, start=start,
+                                           stop=stop)
+                    rdf_list[snap, :] = gr
 
+                rdf_list = np.sum(rdf_list, axis=0) / rdf_list.shape[0]
+                return rdf_list, r
+            if gr_type=="HH":
+                rdf_list = np.zeros((self.n_snapshots, n_bins -1))
+                for snap in range(self.n_snapshots ):
+                    gr, r = calc_rdf_rdist(data=self.s1, box=self.box_size, snapshot=snap, n_bins=n_bins, start=start,
+                                           stop=stop)
+                    rdf_list[snap, :] = gr
 
-                return np.sum(rdf_list, axis=0) / number_density, bin_list
+                rdf_list = np.sum(rdf_list, axis=0) / rdf_list.shape[0]
+                return rdf_list, r
+            if gr_type=="OH":
+                rdf_list = np.zeros((self.n_snapshots, n_bins -1))
+                for snap in range(self.n_snapshots ):
+                    gr, r = calc_rdf_rdist(data=self.s2, box=self.box_size, snapshot=snapshot, n_bins=n_bins, start=start,
+                                           stop=stop, data_2=self.s1[snap])
+                    rdf_list[snap, :] = gr
 
-
+                rdf_list = np.sum(rdf_list, axis=0) / rdf_list.shape[0]
+                return rdf_list, r
 
     def plot_water_hist(self, index_list: np.ndarray = None) -> None:
         '''

testing/md_class_test.py
@@ -1,17 +1,21 @@
+import numpy as np
+
 from testing_cases import read_lammps_test, MSD_test
-from src.tools.md_class_utility import plot_MSD, plot_ion_speed
+from src.tools.md_class_utility import *
+
 import matplotlib.pyplot as plt
 
 trj = read_lammps_test(path="trjwater.lammpstrj", scaled=1)
 #msd = trj.get_MSD()
 #plot_MSD(msd)
-g_r, r= trj.get_rdf_rdist(180, gr_type="OO", n_bins=50, start=0.05, single=False)
+g_r, r= trj.get_rdf_rdist(180, gr_type="HH", n_bins=150, start=0.01, single_frame=False)
 #g_r, r= trj.get_rdf(180, gr_type="OO", n_bins=50, start=0.5, single=True)
 
-plt.plot(r[1:], g_r)
-plt.show()
-
+plot_rdf(g_r, r, "HH")
 #oh, h3 = trj.get_ion_speed()
 #plot_ion_speed(oh, h3)
 
+a = np.array([1, 2, 3, 4])
+b = np.array([2, 4, 5, 6])
 
+print(np.divide(a, b))